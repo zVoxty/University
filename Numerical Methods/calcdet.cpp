c55c070458a7726581388d8f1476f813637f99b2
zVoxty <zvoxty@gmail.com>
2016-03-28 19:06:48 +0300

    Update calcdet.cpp

--- Numerical Methods/calcdet.cpp
+++ Numerical Methods/calcdet.cpp
@@ -2,87 +2,100 @@
 
 using namespace std;
 
-int main(){
-	//1
-	float a[10][10];
-	int n, i, j;
-    int det;
-	
+float a[10][10];
+int i, j;
+
+int main() {
+
+	int val_det;
+	int n; /* n = Number of lines and columns */
+	cout << " Insert number of lines and columns : ";
 	cin >> n;
-	
-	for(i =1; i<= n; i++){
-		for(j = 1; j <= n; j++){
+
+	/* Read Matrix */
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++) {
 			cin >> a[i][j];
 		}
 	}
-	//2
-	det = 1;
-	//3
-	do{
-		//3.1
-		if(a[1][1] == 0){
-			
-			//3.1.1
+	
+	if (a[1][1] == 0) {
+		cout << "First determinant is 0 !\n ";
+	}
+	else {
+	
+		cout << "First determinant is " << a[1][1] << " !\n";
+	}
+
+	if (a[1][1] * a[2][2] - a[1][2] * a[2][1] == 0) {
+		cout << "Second determinant is 0 !\n";
+	}
+	else{
+		cout << "Second determinant is " << a[1][1] * a[2][2] - a[1][2] * a[2][1] << " !\n";
+	}
+
+	/* Set default value */
+	val_det = 1;
+
+	do {
+		/* If first element is 0 search on next lines an element != 0 */
+		if (a[1][1] == 0) {
+			/* Set i = next line */
 			i = 2;
 			
-			//3.1.2
-			while( i <= n && a[i][1] == 0){
+			while (i <= n && a[i][1] == 0) {
 				i++;
 			}
+
 			
-			//3.1.3
-			if(i > n){
+			if (i > n) {
 				cout << "Determinat a = 0 \n";
 				return 0;
 			}
+
 			
-			//3.1.4
-			for(j = 1; j <= n; j++){
+			for (j = 1; j <= n; j++) {
 				float aux = a[1][j];
 				a[1][j] = a[i][j];
 				a[i][j] = aux;
 			}
-			
-			//3.1.5
-			det = -det;
-		}
+
 		
-		//3.2
-		for(i = 1; i <= n - 2; i++){
-			det = det * a[1][1];
+			val_det = -val_det;
 		}
+
 		
-		//3.3
-		for(i = 2; i <= n; i++){
+		for (i = 1; i <= n - 2; i++) {
+			val_det = val_det * a[1][1];
+		}
+
+		
+		for (i = 2; i <= n; i++) {
+			for (j = 2; j <= n; j++) {
+
 			
-			//3.3.1
-			for(j = 2; j <= n; j++){
-				
-				//3.3.1.1
 				a[i][j] = a[i][j] * a[1][1] - a[i][1] * a[1][j];
 			}
 		}
-		
-		//3.4
+
+	
 		n--;
+
 		
-		//3.5
-		for(i = 1; i <= n; i++){
-			
-			//3.5.1
-			for(j = 1; j <= n; j++){
+		for (i = 1; i <= n; i++) {
+
+		
+			for (j = 1; j <= n; j++) {
+
 				
-				//3.5.1.1
 				a[i][j] = a[i + 1][j + 1];
 			}
 		}
-	}while(n != 1);
-	
-	//4
-	det = a[1][1]/det;
-	
-	//5
-	cout << "Determinantul este egal cu = "<<det;
-	
-	return 0;
+	} while (n != 2);
+
+		val_det = a[1][1] / val_det;
+
+		cout << "Third determinant is " << val_det << " !\n";
+
+	system("PAUSE");
 }
